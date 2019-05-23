42e1a18dd690ccb16ab6a5e7fcd8d54cac226c9a
ordo <ordogod00@gmail.com>
2019-05-23 18:04:33 +0300

    Binded layout name changed

--- app/src/main/java/com/ordolabs/clipit/ui/home/HomeActivity.java
+++ app/src/main/java/com/ordolabs/clipit/ui/home/HomeActivity.java
@@ -12,7 +12,7 @@ public class HomeActivity extends BaseActivity implements HomeMvpContract.View {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         mvpPresenter = new HomePresenter<>(this);
     }
