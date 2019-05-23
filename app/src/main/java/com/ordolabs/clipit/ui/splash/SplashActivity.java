08565796d35e04ac4135b8fa7c07411b9c8b7bc6
ordo <ordogod00@gmail.com>
2019-05-23 18:44:22 +0300

    Splash screen added

---
+++ app/src/main/java/com/ordolabs/clipit/ui/splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.ordolabs.clipit.ui.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ordolabs.clipit.ui.base.BaseActivity;
+import com.ordolabs.clipit.ui.home.HomeActivity;
+
+/**
+ * Created by ordogod on 23.05.19.
+ **/
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try { //TODO: remove sleep
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(HomeActivity.getStartingIntent(this));
+        finish();
+    }
+}
