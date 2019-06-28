82f30f49779c5ba574e2eb86c944faf3fca1203a
ordo <ordogod00@gmail.com>
2019-06-28 20:00:20 +0300

    New ClipActivity added and registered

--- app/src/main/java/com/ordolabs/clipit/ui/clip/ClipActivity.java
+++ app/src/main/java/com/ordolabs/clipit/ui/clip/ClipActivity.java
@@ -1,6 +1,9 @@
 package com.ordolabs.clipit.ui.clip;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.ordolabs.clipit.ui.base.BaseActivity;
@@ -11,10 +14,19 @@ import com.ordolabs.clipit.ui.base.BaseActivity;
 
 public class ClipActivity extends BaseActivity implements ClipMvpContract.View {
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    public static Intent getStartingIntent(@NonNull Context callingContext) {
+        return new Intent(callingContext, ClipActivity.class);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
