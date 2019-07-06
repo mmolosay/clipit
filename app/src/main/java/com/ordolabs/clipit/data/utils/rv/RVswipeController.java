68f2c3ec7a84ecb0bcdc48941074682db6f4c13d
ordo <ordogod00@gmail.com>
2019-07-06 21:29:03 +0300

    Made RV items "unswipeable"

--- app/src/main/java/com/ordolabs/clipit/data/utils/rv/RVswipeController.java
+++ app/src/main/java/com/ordolabs/clipit/data/utils/rv/RVswipeController.java
@@ -1,8 +1,12 @@
 package com.ordolabs.clipit.data.utils.rv;
 
+import android.annotation.SuppressLint;
+import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper.Callback;
+import android.view.MotionEvent;
+import android.view.View;
 
 import static android.support.v7.widget.helper.ItemTouchHelper.*;
 
@@ -11,6 +15,8 @@ import static android.support.v7.widget.helper.ItemTouchHelper.*;
  **/
 
 public class RVswipeController extends Callback {
+
+    private boolean swipeBack;
 
     @Override
     public int getMovementFlags(@NonNull RecyclerView recyclerView,
@@ -34,5 +40,42 @@ public class RVswipeController extends Callback {
             int direction)
     {
 
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c,
+                            @NonNull RecyclerView recyclerView,
+                            @NonNull RecyclerView.ViewHolder viewHolder,
+                            float dX,
+                            float dY,
+                            int actionState,
+                            boolean isCurrentlyActive)
+    {
+
+        if (actionState == ACTION_STATE_SWIPE) {
+            setTouchListener(recyclerView);
+        }
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setTouchListener(RecyclerView recyclerView) {
+
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                swipeBack = event.getAction() == MotionEvent.ACTION_CANCEL || event.getAction() == MotionEvent.ACTION_UP;
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        if (swipeBack) {
+            swipeBack = false;
+            return 0;
+        }
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
     }
 }
