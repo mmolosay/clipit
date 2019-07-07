9ff01c7e2162ab9819f273d0cf580a3c4af8196c
ordo <ordogod00@gmail.com>
2019-07-07 23:57:06 +0300

    Deleting BG on RV item swipe improved

--- app/src/main/java/com/ordolabs/clipit/data/utils/rv/RVswipeController.java
+++ app/src/main/java/com/ordolabs/clipit/data/utils/rv/RVswipeController.java
@@ -1,14 +1,16 @@
 package com.ordolabs.clipit.data.utils.rv;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper.Callback;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.ordolabs.clipit.ClipItApplication;
@@ -24,16 +26,44 @@ import static android.support.v7.widget.helper.ItemTouchHelper.*;
 public class RVswipeController extends Callback {
 
     private RVadapter adapter;
-    private ColorDrawable swipeBG;
+    private View itemView;
     private Drawable deleteIcon;
+    private Paint paint;
 
     private ClipRaw clipRemoved;
     private int clipPosition;
 
+    private float deleteIconScale = 1.5f;
+    private int deleteIconWidth;
+    private int deleteIconHeight;
+    private int deleteIconCenteringOffset;
+    private int deleteIconMargin;
+    private float swipeBGroundness;
+
     public RVswipeController(RVadapter adapter) {
         this.adapter = adapter;
-        this.swipeBG = new ColorDrawable(Color.parseColor("#ff0000"));
-        this.deleteIcon = ContextCompat.getDrawable(ClipItApplication.getAppContext(), R.drawable.ic_delete_light_24dp);
+
+        Drawable deleteIcon = ContextCompat.getDrawable(ClipItApplication.getAppContext(), R.drawable.ic_delete_light_24dp);
+        assert deleteIcon != null;
+
+        this.deleteIconWidth = Math.round(deleteIcon.getIntrinsicWidth() * deleteIconScale);
+        this.deleteIconHeight = Math.round(deleteIcon.getIntrinsicHeight() * deleteIconScale);
+
+        this.deleteIcon = new ScaleDrawable(
+                deleteIcon,
+                0,
+                deleteIconWidth,
+                deleteIconHeight
+        ).getDrawable();
+
+        this.paint = new Paint();
+        this.paint.setColor(ContextCompat.getColor(ClipItApplication.getAppContext(), R.color.delete_item_bg));
+
+        this.swipeBGroundness = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                2,
+                ClipItApplication.getAppContext().getResources().getDisplayMetrics()
+        );
     }
 
     @Override
@@ -46,8 +76,8 @@ public class RVswipeController extends Callback {
     @Override
     public boolean onMove(
             @NonNull RecyclerView recyclerView,
-            @NonNull  RecyclerView.ViewHolder viewHolder,
-            @NonNull  RecyclerView.ViewHolder target)
+            @NonNull RecyclerView.ViewHolder viewHolder,
+            @NonNull RecyclerView.ViewHolder target)
     {
         return false;
     }
@@ -61,11 +91,11 @@ public class RVswipeController extends Callback {
         clipRemoved = adapter.deleteItem(clipPosition);
 
         Snackbar
-        .make(
-                viewHolder.itemView,
-                R.string.clipDeleteSnackbarNotification,
-                Snackbar.LENGTH_LONG
-        ).setAction(
+                .make(
+                        viewHolder.itemView,
+                        R.string.clipDeleteSnackbarNotification,
+                        Snackbar.LENGTH_LONG
+                ).setAction(
                 R.string.clipDeleteSnackbarAction,
                 new View.OnClickListener() {
                     @Override
@@ -94,19 +124,59 @@ public class RVswipeController extends Callback {
                             int actionState,
                             boolean isCurrentlyActive)
     {
-        View itemView = viewHolder.itemView;
+        if (dX == 0) super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        this.itemView = viewHolder.itemView;
+        this.deleteIconMargin = (itemView.getHeight() - deleteIconHeight) / 2;
+        this.deleteIconCenteringOffset =
+                Math.abs(dX) > deleteIconWidth + deleteIconMargin * 2 ?
+                Math.round(Math.abs(dX) - deleteIconWidth - deleteIconMargin * 2) / 2 :
+                0;
+
         if (dX > 0) {
-            swipeBG.setBounds(
-                    itemView.getLeft() - ((RecyclerView.LayoutParams) itemView.getLayoutParams()).leftMargin,
-                    itemView.getTop(),
-                    (int) dX, itemView.getBottom()
+            deleteIcon.setBounds(
+                    itemView.getLeft() + deleteIconMargin + deleteIconCenteringOffset,
+                    itemView.getTop() + deleteIconMargin,
+                    itemView.getLeft() + deleteIconMargin + deleteIconWidth + deleteIconCenteringOffset,
+                    itemView.getBottom() - deleteIconMargin
             );
+            c.clipRect(
+                    itemView.getLeft(),
+                    itemView.getTop(),
+                    (int) dX,
+                    itemView.getBottom()
+            );
+
+            c.drawRoundRect(new RectF(
+                    itemView.getLeft(),
+                    itemView.getTop(),
+                    (int) dX < itemView.getRight() ? (int) dX : itemView.getRight(),
+                    itemView.getBottom()
+            ), swipeBGroundness, swipeBGroundness, paint);
         }
         else {
+            deleteIcon.setBounds(
+                    itemView.getRight() + (int) dX + deleteIconCenteringOffset,
+                    itemView.getTop() + deleteIconMargin,
+                    itemView.getLeft() + deleteIconMargin + deleteIconWidth + deleteIconCenteringOffset,
+                    itemView.getBottom() - deleteIconMargin
+            );
+            c.clipRect(
+                    itemView.getLeft(),
+                    itemView.getTop(),
+                    (int) dX,
+                    itemView.getBottom()
+            );
 
+            c.drawRoundRect(new RectF(
+                    (int) dX < itemView.getRight() ? (int) dX : itemView.getRight(),
+                    itemView.getTop(),
+                    itemView.getRight(),
+                    itemView.getBottom()
+            ), swipeBGroundness, swipeBGroundness, paint);
         }
 
-        swipeBG.draw(c);
+        deleteIcon.draw(c);
 
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
