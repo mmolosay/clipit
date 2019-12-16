c4fb19d16bf3fb45dddf4967d8f2ae7c47716c35
ordo <ordogod00@gmail.com>
2019-12-16 22:03:31 +0300

    Move data binding to ViewHolder

--- app/src/main/java/com/ordolabs/clipit/util/clipRV/ClipItemViewHolder.java
+++ app/src/main/java/com/ordolabs/clipit/util/clipRV/ClipItemViewHolder.java
@@ -1,10 +1,17 @@
 package com.ordolabs.clipit.util.clipRV;
 
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ordolabs.clipit.App;
 import com.ordolabs.clipit.R;
+import com.ordolabs.clipit.util.PrettyDate;
 
 /**
  * Created by ordogod on 18.06.19.
@@ -20,5 +27,33 @@ final class ClipItemViewHolder extends RecyclerView.ViewHolder {
 
         textView = view.findViewById(R.id.RVclipsItemText);
         dateView = view.findViewById(R.id.RVclipsItemDate);
+    }
+
+    void bindData(Clip clip) {
+        // replaces all \n sequences with one
+        String trimmed = clip.text.trim().replaceAll("[\n]{2,}", "\n");
+        int titleLength = getTitleLength(trimmed);
+
+        SpannableStringBuilder styledText = new SpannableStringBuilder(trimmed);
+        styledText.setSpan(
+                new StyleSpan(Typeface.BOLD),
+                0, titleLength,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        styledText.setSpan(
+                new ForegroundColorSpan(App.getContext().getResources().getColor(R.color.textPrimary)),
+                0, titleLength,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        this.textView.setText(styledText);
+
+        this.dateView.setText(PrettyDate.from(clip.date));
+    }
+
+    private int getTitleLength(String clipText) {
+        for (int i = 0; i < clipText.length() ; i++)
+            if (clipText.charAt(i) == '\n')
+                return i;
+        return clipText.length();
     }
 }
--- app/src/main/java/com/ordolabs/clipit/util/clipRV/ClipRVadapter.java
+++ app/src/main/java/com/ordolabs/clipit/util/clipRV/ClipRVadapter.java
@@ -1,26 +1,19 @@
 package com.ordolabs.clipit.util.clipRV;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
-import com.ordolabs.clipit.App;
 import com.ordolabs.clipit.R;
 import com.ordolabs.clipit.data.C;
 import com.ordolabs.clipit.data.realm.RealmDealer;
-import com.ordolabs.clipit.ui.clip.ClipActivity;
-import com.ordolabs.clipit.util.PrettyDate;
+import com.ordolabs.clipit.ui.edit.EditActivity;
 
 import java.util.ArrayList;
 
@@ -45,70 +38,58 @@ public class ClipRVadapter extends RecyclerView.Adapter<ClipItemViewHolder> {
         View view = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.clip_card_item, parent, false);
-        view.setOnClickListener(newOnClickListener(view));
+        view.setOnClickListener(makeClipOnClickListener(view));
 
         return new ClipItemViewHolder(view);
     }
 
-    private View.OnClickListener newOnClickListener(final View view) {
-        return v -> caller.startActivity(new Intent(
-                caller,
-                ClipActivity.class
-            ).putExtra(
-                C.EXTRA_CLIP_ID,
-                clips.get(rv.getChildLayoutPosition(view)).id
-            )
-        );
-    }
-
     @Override
     public void onBindViewHolder(@NonNull final ClipItemViewHolder holder, int i) {
-        setTextViews(holder, clips.get(i));
-//        setNewDriverMark(holder.newDriverMark, clips.get(i));
+        holder.bindData(clips.get(i));
+        //setNewDriverMark(holder.newDriverMark, clips.get(i));
     }
 
-    Clip deleteItem(final int position) {
+    Clip removeClip(final int position) {
         Clip clip = clips.get(position);
 
         this.clips.remove(position);
         notifyItemRemoved(position);
-        notifyItemRangeChanged(position, getItemCount());
 
         return clip;
     }
 
-    void restoreItem(final int position, Clip clip) {
+    void restoreClip(final int position, Clip clip) {
         this.clips.add(position, clip);
         notifyItemInserted(position);
 
         rv.post(() -> rv.smoothScrollToPosition(position));
     }
 
-    private void setTextViews(ClipItemViewHolder holder, Clip clip) {
-        String trimmed = clip.text.trim().replaceAll("[\n]{2,}", "\n");
-        int titleLength = getTitleLength(trimmed);
-
-        SpannableStringBuilder text = new SpannableStringBuilder(trimmed);
-        text.setSpan(
-                new StyleSpan(Typeface.BOLD),
-                0, titleLength,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        );
-        text.setSpan(
-                new ForegroundColorSpan(App.getContext().getResources().getColor(R.color.textPrimary)),
-                0, titleLength,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        );
-        holder.textView.setText(text);
-
-        holder.dateView.setText(PrettyDate.from(clip.date));
+    @Override
+    public int getItemCount() {
+        if (clips != null) return clips.size();
+        return 0;
     }
 
-    private int getTitleLength(String clipText) {
-        for (int i = 0; i < clipText.length() ; i++)
-            if (clipText.charAt(i) == '\n')
-                return i;
-        return clipText.length();
+    public void setClips(ArrayList<Clip> clips) {
+        this.clips.clear();
+        this.clips.addAll(clips);
+        notifyDataSetChanged();
+    }
+
+    public int getClipsCount() {
+        return clips.size();
+    }
+
+    private View.OnClickListener makeClipOnClickListener(final View view) {
+        return v -> caller.startActivity(new Intent(
+                        caller,
+                        EditActivity.class
+                ).putExtra(
+                C.EXTRA_CLIP_ID,
+                clips.get(rv.getChildLayoutPosition(view)).id
+                )
+        );
     }
 
     private void setNewDriverMark(View mark, Clip clip) {
@@ -132,18 +113,4 @@ public class ClipRVadapter extends RecyclerView.Adapter<ClipItemViewHolder> {
             clip.isViewed = true;
         }
     }
-
-    @Override
-    public int getItemCount() {
-        if (clips != null) return clips.size();
-        return 0;
-    }
-
-    public void setClips(ArrayList<Clip> clips) {
-        this.clips.clear();
-        this.clips.addAll(clips);
-        notifyDataSetChanged();
-    }
-
-    public int getClipsCount() { return clips.size(); }
 }
